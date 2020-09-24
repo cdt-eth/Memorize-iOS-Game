4448e8901b5ef16fbbeb23d0b29de00a71201633
christian <christiandavidphoto@gmail.com>
2020-09-24 11:49:37 -0400

    added model file

---
+++ Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,22 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Christian David Turner on 9/24/20.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent>{
+    var cards: Array<Card>
+    
+    func choose(card: Card){
+        print("card chosen: \(card)")
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}

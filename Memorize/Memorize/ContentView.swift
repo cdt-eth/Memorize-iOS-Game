41cac3b031ae693f4431d1455994db73939fa1e9
christian <christiandavidphoto@gmail.com>
2020-09-24 10:47:45 -0400

    added ForEach

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,10 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack(content: {
-            RoundedRectangle(cornerRadius: 10.0).stroke()
-            Text("🦂")
-        })
+        return HStack(content: {
+        ForEach(0..<4, content:{ index in
+            ZStack(content: {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("🦂")
+            })
+        })})
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
         
     }
 }

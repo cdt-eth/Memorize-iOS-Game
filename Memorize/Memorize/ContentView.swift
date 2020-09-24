d0d88e4930dc837cc52ec0b8ba83672f72d94066
christian <christiandavidphoto@gmail.com>
2020-09-24 11:01:31 -0400

    added isFaceUp bool

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -11,22 +11,32 @@ struct ContentView: View {
     var body: some View {
         return HStack {
             ForEach(0..<4) { index in
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                    Text("🦂")
-                }
+                CardView(isFaceUp: true)
             }
         }
         .padding()
         .foregroundColor(Color.orange)
         .font(Font.largeTitle)
-        
     }
 }
 
 
-
+struct CardView: View {
+    
+    var isFaceUp: Bool
+    
+    var body: some View{
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("🦂")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
 
 
 

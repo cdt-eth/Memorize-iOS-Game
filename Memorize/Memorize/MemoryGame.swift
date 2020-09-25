484e7146329a0af26477810084147e22a1a5f9d1
christian <christiandavidphoto@gmail.com>
2020-09-25 15:13:18 -0400

    gameplay logic

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -6,13 +6,29 @@
 
 import Foundation
 
-struct MemoryGame<CardContent>{
+struct MemoryGame<CardContent> where CardContent: Equatable{
     var cards: Array<Card>
+    
+    var indexOfTheOneAndOnlyFaceUpCard: Int? // optinals are automatically initialized to nil
     
     mutating func choose(card: Card){
         print("card chosen: \(card)")
-        if let chosenIndex = cards.firstIndex(matching: card) {
-            cards[chosenIndex].isFaceUp = !cards[chosenIndex].isFaceUp
+        
+        // the comma here is the same as &&, it's called "sequential and" for "let's"
+        if let chosenIndex = cards.firstIndex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard{
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content{
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFaceUpCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp = true
         }
     }
     
@@ -31,7 +47,7 @@ struct MemoryGame<CardContent>{
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int

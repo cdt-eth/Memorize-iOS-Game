210085b82e3e6ca240d7830d755aaf65bffbac10
christian <christiandavidphoto@gmail.com>
2020-09-28 14:43:43 -0400

    cardify

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -10,22 +10,22 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-//
-//        Text("Animals")
-//            .font(.largeTitle).padding()
-//
-//        HStack {
-//            Text("Score: 12")
-//                .font(.body).bold().padding(8.0)
-//
-//            Button(action: {
-//                print("NEW GAME")
-//            }) {
-//                Text("NEW GAME")
-//                    .font(.body).bold().padding(8.0)
-//                    .overlay(RoundedRectangle(cornerRadius: 35.0).stroke(lineWidth: 2.0))
-//            }
-//        }
+        //
+        //        Text("Animals")
+        //            .font(.largeTitle).padding()
+        //
+        //        HStack {
+        //            Text("Score: 12")
+        //                .font(.body).bold().padding(8.0)
+        //
+        //            Button(action: {
+        //                print("NEW GAME")
+        //            }) {
+        //                Text("NEW GAME")
+        //                    .font(.body).bold().padding(8.0)
+        //                    .overlay(RoundedRectangle(cornerRadius: 35.0).stroke(lineWidth: 2.0))
+        //            }
+        //        }
         
         Grid(viewModel.cards) { card in
             CardView(card: card).onTapGesture{
@@ -61,30 +61,26 @@ struct CardView: View {
         }
     }
     
+    @ViewBuilder
     private func body(for size: CGSize) -> some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-                
-                // in iOS "0 degrees" isn't true norht, it's our 3 oclock
+        
+        if card.isFaceUp || !card.isMatched{
+            ZStack {
+                // in iOS "0 degrees" isn't true north, it's our 3 oclock
                 // in iOS clockwise is opposite
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90),clockwise: true).padding(5).opacity(0.4)
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true).padding(5).opacity(0.4)
                 
                 Text(card.content)
-            } else {
-                if !card.isMatched{
-                    RoundedRectangle(cornerRadius: cornerRadius).fill()
-                }
+                    .font(Font.system(size: fontSize(for: size)))
             }
-        }.font(Font.system(size: fontSize(for: size)))
+            .cardify(isFaceUp:card.isFaceUp)
+        }
     }
     
     
     // MARK: - Drawing Constants
     
-    private let cornerRadius: CGFloat = 10.0
-    private let edgeLineWidth: CGFloat = 3
+    
     
     private func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * 0.7
